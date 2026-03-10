e52fa92b25a12d2aa60319e86b8f3c4ffe1fd32f
Nisarg Shah <nisargshah8b@gmail.com>
2026-03-10 08:49:01 -0700

    Fix tcp_benchmark CMake and preprocessor directives

--- benchmarks/tcp_benchmark.cpp
+++ benchmarks/tcp_benchmark.cpp
@@ -14,6 +14,12 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
+#else
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#endif
 
 namespace {
 
